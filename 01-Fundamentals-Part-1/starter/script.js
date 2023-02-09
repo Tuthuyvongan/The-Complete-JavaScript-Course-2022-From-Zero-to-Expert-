1ab87db8a477c45e30f0b4dcb28433bc5ca3416f
Tuthuyvongan <48273012+Tuthuyvongan@users.noreply.github.com>
2023-02-09 21:28:26 +0700

    Update script

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -94,17 +94,41 @@
 // }
 // console.log(century);
 //type conversation
-const inputYear = 1991;
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
-console.log(Number("Jonas"));
-console.log(typeof NaN);
-console.log(String(23), 23);
-//type Coercion
-console.log("I am " + 23 + " years old");
-console.log("23" - "10" - 3);
-console.log("23" / 2);
+// const inputYear = 1991;
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
+// console.log(String(23), 23);
+// //type Coercion
+// console.log("I am " + 23 + " years old");
+// //Dấu trừ sẽ biến kiểu dữ liệu từ string thành number
+// console.log("23" - "10" - 3);
+// console.log("23" / 2);
+// console.log("23" * 2);
 
-let n = "1" + 1;
-n = n - 1;
-console.log(n);
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+
+const money = 0;
+// if chỉ thực thi khi true
+if (money) {
+  console.log("Don't spend it all");
+} else {
+  console.log("You shound get a job!");
+}
+
+let height = 0;
+if (height) {
+  console.log("Yay! Height is defined");
+} else {
+  console.log("Height is undefined");
+}
