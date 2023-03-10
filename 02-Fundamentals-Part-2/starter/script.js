9a21ed33553602e2fef6d3e445f454ce0f386c72
Tuthuyvongan <48273012+Tuthuyvongan@users.noreply.github.com>
2023-03-10 22:41:40 +0700

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -109,4 +109,47 @@
 // console.log(yearsUntilRetirement(1950, "Mike"));
 
 ////////////////////////////////////
-//
+// Arrays
+// const friend1 = "Michael";
+// const friend2 = "Steven";
+// const friend3 = "Peter";
+
+// const friends = ["Michael", "Steven", "Peter"];
+// console.log(friends);
+
+// const year = new Array(1999, 1984, 2008, 2020);
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = "Jay";
+// console.log(friends);
+// // friends = ["Michael", "Jay", "Peter"];
+
+// const firstName = "Jonas";
+// const jonas = [firstName, "Schmedtmann", 2037 - 1971, "teacher", friends];
+// console.log(jonas);
+
+// // Exercise
+// const calcAge = function (birthYear) {
+//   return 2023 - birthYear;
+// };
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[2]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [
+//   calcAge(years[0]),
+//   calcAge(years[2]),
+//   calcAge(years[years.length - 1]),
+// ];
+// console.log(ages);
+
+////////////////////////////////////
+// Basic Array Operations
