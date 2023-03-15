c5c4c0bb0734917ca0f652a2b0000a725d040d91
Tuthuyvongan <48273012+Tuthuyvongan@users.noreply.github.com>
2023-03-15 21:13:01 +0700

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -153,3 +153,33 @@
 
 ////////////////////////////////////
 // Basic Array Operations
+// const friends = ["Michael", "Steven", "Peter"];
+
+// //Add elements to an array
+// const newLength = friends.push("Jay");
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift("John");
+// console.log(friends);
+
+// //Remove elements
+// friends.pop(); //Last
+// const popped = friends.pop();
+// console.log(friends);
+// console.log(popped);
+
+// friends.shift(); //First
+// console.log(friends);
+
+// console.log(friends.indexOf("Steven"));
+// console.log(friends.indexOf("Bob"));
+
+// friends.push(23);
+// console.log(friends.includes("Steven"));
+// console.log(friends.includes("Bob"));
+// console.log(friends.includes(23));
+
+// if (friends.includes("Steven")) {
+//   console.log("You have a friend called Steven");
+// }
