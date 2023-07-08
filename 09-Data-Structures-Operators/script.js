2d61152a0e4c0d8e20e7dcf8a4c89b85cf60c967
Tuthuyvongan <48273012+Tuthuyvongan@users.noreply.github.com>
2023-07-08 12:39:59 +0700

    Revert "more exercise"
    This reverts commit 268a4b9d3f94bd7b8c2eecc4010794e4a9873226.

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -18,6 +18,9 @@ const openingHours = {
     close: 24,
   },
 };
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
@@ -48,14 +51,6 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
-
-//////////////////////////////////////
-// String Methods Practice
-
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-let arr = flights.split('+_');
-console.log(arr);
 
 //////////////////////////////////////
 // Working With Strings - Parts 3
